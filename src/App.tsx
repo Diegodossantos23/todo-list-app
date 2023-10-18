6dbf9384ab86f129a10a7995839bb9af090abca1
Diegodossantos23 <diegodossantos900@outlook.com>
2023-10-18 13:05:04 -0300

    feat: create project structure

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import TaskList from './components/TaskList/TaskList';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
+
+
+
